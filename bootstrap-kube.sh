a507775d475afb6d83a4c742ec6b5046e6b2147d
Uppala Sandeep Chowdary <sandeepchowdaryu@gmail.com>
2020-01-05 00:46:52 +0530

    Update bootstrap-kube.sh

--- bootstrap-kube.sh
+++ bootstrap-kube.sh
@@ -5,6 +5,8 @@ echo "[TASK 1] Install docker container engine"
 yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
 yum install -y -q docker-ce-18.06.0.ce-3.el7 >/dev/null 2>&1
+#Install on all nodes for nfs-dynamic provisoning
+yum install -y nfs-utils
 
 # Enable docker service
 echo "[TASK 2] Enable and start docker service"
