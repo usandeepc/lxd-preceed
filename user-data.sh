8d1c152d4cd5886a3f6573b4499122caaba95154
Uppala Sandeep Chowdary <sandeepchowdaryu@gmail.com>
2020-10-23 00:11:03 +0530

    Created branch for lxd virtual machine support
    Created branch for lxd virtual machine support.
    Changed Base OS to ubuntu:20.04

--- user-data.sh
+++ user-data.sh
@@ -1,18 +1,18 @@
 #Install and configure Lxd
-sudo apt-get install lxd
+#Required Ubuntu:20.04
+sudo snap install lxd
 sudo gpasswd -a ubuntu lxd
-wget -O preseed.yaml https://raw.githubusercontent.com/usandeepc/lxd-preceed/master/lxd-preceed.yaml
-cat preseed.yaml | lxd init --preseed
-#lxc list
-lxc profile create k8s
-wget -O lxc-profile-k8s.yaml https://raw.githubusercontent.com/usandeepc/lxd-preceed/master/lxc-profile-k8s.yaml
-cat lxc-profile-k8s.yaml | lxc profile edit k8s
-lxc launch images:centos/7 kmaster --profile k8s
-lxc launch images:centos/7 kworker1 --profile k8s
-lxc launch images:centos/7 kworker2 --profile k8s
-lxc launch images:centos/7 kworker3 --profile k8s
+lxc launch images:centos/7 kmaster --vm
+lxc launch images:centos/7 kworker1 --vm
+lxc launch images:centos/7 kworker2 --vm
+lxc launch images:centos/7 kworker3 --vm
 wget -O bootstrap-kube.sh https://raw.githubusercontent.com/usandeepc/lxd-preceed/master/bootstrap-kube.sh
-sleep 60
+#Delay is required to initialize lxd-agent on vms
+sleep 120
+lxc exec kmaster -- hostnamectl set-hostname kmaster
+lxc exec kworker1 -- hostnamectl set-hostname kworker1
+lxc exec kworker2 -- hostnamectl set-hostname kworker2
+lxc exec kworker3 -- hostnamectl set-hostname kworker3
 cat bootstrap-kube.sh | lxc exec kmaster bash
 cat bootstrap-kube.sh | lxc exec kworker1 bash
 cat bootstrap-kube.sh | lxc exec kworker2 bash
